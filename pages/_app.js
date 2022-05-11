0309ad76873ed0659cc0a2c2ee9e932884eeba5d
James Lee <lee.jamests@gmail.com>
2022-05-11 14:44:17 -0400

    Initial sign in sign out page.
    Twitch OAuth added. Email functionality added, but will need a smtp service.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { SessionProvider } from 'next-auth/react'
+import { useSession } from 'next-auth/react' 
+import { Toaster } from 'react-hot-toast'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ 
+  Component, 
+  pageProps: { session, ...pageProps } }) {
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />  
+      </SessionProvider>
+
+      <Toaster />
+    </>
+  )
 }
 
 export default MyApp
